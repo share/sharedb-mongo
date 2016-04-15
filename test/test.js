a77187ca5acb730565321ceefb244f438783620a
Avital Oliver <avital@theWE.net>
2016-04-14 23:53:35 -0700

    Test queries with $count

--- test/test.js
+++ test/test.js
@@ -143,6 +143,27 @@ describe('mongo db', function() {
       });
     });
 
+    it('$count should count documents', function(done) {
+      var snapshots = [
+        {type: 'json0', v: 1, data: {x: 1, y: 1}},
+        {type: 'json0', v: 1, data: {x: 2, y: 2}},
+        {type: 'json0', v: 1, data: {x: 3, y: 2}}
+      ];
+      var db = this.db;
+      db.commit('testcollection', 'test1', {v: 0, create: {}}, snapshots[0], function(err) {
+        db.commit('testcollection', 'test2', {v: 0, create: {}}, snapshots[1], function(err) {
+          db.commit('testcollection', 'test3', {v: 0, create: {}}, snapshots[2], function(err) {
+            var query = {$count: true, $query: {y: 2}};
+            db.query('testcollection', query, null, null, function(err, results, extra) {
+              if (err) throw err;
+              expect(extra).eql(2);
+              done();
+            });
+          });
+        });
+      });
+    });
+
     it('$distinct should perform distinct operation', function(done) {
       var snapshots = [
         {type: 'json0', v: 1, data: {x: 1, y: 1}},
