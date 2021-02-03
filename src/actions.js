96ed2aaadbbf331a7949eba0cfadaacdac5afb18
Christina Burger <christina.burger@lever.co>
2021-02-03 16:22:35 -0500

    Small wording fix in comments

--- src/actions.js
+++ src/actions.js
@@ -1,6 +1,6 @@
 module.exports = {
   // Triggers before the call to replace a document is made to Mongo
   beforeEdit: 'beforeEdit',
-  // Triggers directly before the call to issue a query to Mongo for a single snapshot by ID
+  // Triggers directly before the call to issue a query to Mongo for snapshots by ID
   beforeSnapshotLookup: 'beforeSnapshotLookup'
 };
