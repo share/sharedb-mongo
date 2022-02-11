d025a8b35094cc252b461892d32e99b192d4951f
Craig Beck <craig.beck@lever.co>
2022-02-11 13:51:41 -0800

    tests ensuring behaviour for getOpts with strict linking on or off

--- test/test_get_ops.js
+++ test/test_get_ops.js
@@ -4,11 +4,12 @@ var sinon = require('sinon');
 
 var mongoUrl = process.env.TEST_MONGO_URL || 'mongodb://localhost:27017/test';
 
-function create(callback) {
-  var db = new ShareDbMongo(mongoUrl, {
+function create(options, callback) {
+  const opts = Object.assign({
     mongoOptions: {},
     getOpsWithoutStrictLinking: true
-  });
+  }, options);
+  const db = new ShareDbMongo(mongoUrl, opts);
   db.getDbs(function(err, mongo) {
     if (err) return callback(err);
     mongo.dropDatabase(function(err) {
@@ -18,10 +19,96 @@ function create(callback) {
   });
 };
 
-describe('getOps', function() {
+describe('getOps with strict linking', function() {
   beforeEach(function(done) {
     var self = this;
-    create(function(err, db, mongo) {
+    create({ getOpsWithoutStrictLinking: true }, function(err, db, mongo) {
+      if (err) return done(err);
+      self.db = db;
+      self.mongo = mongo;
+      done();
+    });
+  });
+
+  afterEach(function(done) {
+    this.db.close(done);
+  });
+
+  describe('a chain of ops', function() {
+    var db;
+    var mongo;
+    var id;
+    var collection;
+
+    beforeEach(function(done) {
+      db = this.db;
+      mongo = this.mongo;
+      id = 'document1';
+      collection = 'testcollection';
+
+      sinon.spy(db, '_getOps');
+      sinon.spy(db, '_getSnapshotOpLink');
+
+      var ops = [
+        {v: 0, create: {}},
+        {v: 1, p: ['foo'], oi: 'bar'},
+        {v: 2, p: ['foo'], oi: 'baz'},
+        {v: 3, p: ['foo'], oi: 'qux'}
+      ];
+
+      commitOpChain(db, mongo, collection, id, ops, function(error) {
+        if (error) done(error);
+        mongo.collection('o_' + collection).deleteOne({v: 1}, done);
+      });
+    });
+
+    it('fetches ops 2-3 without fetching all ops', function(done) {
+      db.getOps(collection, id, 2, 4, null, function(error, ops) {
+        if (error) return done(error);
+        expect(ops.length).to.equal(2);
+        expect(ops[0].v).to.equal(2);
+        expect(ops[1].v).to.equal(3);
+        done();
+      });
+    });
+
+    it('default option errors when missing ops', function(done) {
+      db.getOps(collection, id, 0, 4, null, function(error) {
+        expect(error.code).to.equal(5103);
+        expect(error.message).to.equal('Missing ops from requested version testcollection.document1 0');
+        done();
+      });
+    });
+
+    it('ignoreMissingOps option returns ops up to the first missing op', function(done) {
+      db.getOps(collection, id, 0, 4, {ignoreMissingOps: true}, function(error, ops) {
+        if (error) return done(error);
+        expect(ops.length).to.equal(2);
+        expect(ops[0].v).to.equal(2);
+        expect(ops[1].v).to.equal(3);
+        done();
+      });
+    });
+
+    it('getOpsToSnapshot ignoreMissingOps option returns ops up to the first missing op', function(done) {
+      db.getSnapshot(collection, id, {$submit: true}, null, function(error, snapshot) {
+        if (error) done(error);
+        db.getOpsToSnapshot(collection, id, 0, snapshot, {ignoreMissingOps: true}, function(error, ops) {
+          if (error) return done(error);
+          expect(ops.length).to.equal(2);
+          expect(ops[0].v).to.equal(2);
+          expect(ops[1].v).to.equal(3);
+          done();
+        });
+      });
+    });
+  });
+});
+
+describe('getOps without strict linking', function() {
+  beforeEach(function(done) {
+    var self = this;
+    create({ getOpsWithoutStrictLinking: false }, function(err, db, mongo) {
       if (err) return done(err);
       self.db = db;
       self.mongo = mongo;
